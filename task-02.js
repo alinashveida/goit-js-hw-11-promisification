7f22abe79f3df9682415bcb642fbe0e47265231a
alinashveida <alinashveida@gmail.com>
2021-05-09 22:33:39 +0300

    2

--- task-02.js
+++ task-02.js
@@ -5,30 +5,10 @@ const users = [
     { name: 'Lux', active: false },
   ];
   
-//   const toggleUserState = (allUsers, userName, callback) => {
-//     const updatedUsers = allUsers.map(user =>
-//       user.name === userName ? { ...user, active: !user.active } : user,
-//     );
-  
-//     callback(updatedUsers);
-//   };
-  
-//   const logger = updatedUsers => console.table(updatedUsers);
-  
-//   /*
-//    * Сейчас работает так
-//    */
-//   toggleUserState(users, 'Mango', logger);
-//   toggleUserState(users, 'Lux', logger);
-  
-  /*
-   * Должно работать так
-   */
-//   toggleUserState(users, 'Mango').then(logger);
-//   toggleUserState(users, 'Lux').then(logger);
+
 
   const toggleUserState = (allUsers, userName) => {
-      const promise = new Promise((resolve, reject ) => {
+      return new Promise((resolve) => {
         const updatedUsers = allUsers.map(user =>
             user.name === userName ? { ...user, active: !user.active }: user,
           );
@@ -39,15 +19,5 @@ const users = [
   const logger = updatedUsers => console.table(updatedUsers);
 
   toggleUserState(users, 'Mango').then(logger);
- toggleUserState(users, 'Lux').then(logger);
+  toggleUserState(users, 'Lux').then(logger);
 
-//   function updatedUsers(allUsers){
-//       allUsers.map(user){
-//           if(user.name === userName){
-//             ...user, active: !user.active
-//           }
-//           else{
-//             user
-//           }
-//       }
-//   }
